30cfe5695fb15d61578af47188e3fc0c87ea2765
Austin Mazur <mazuraustin1@gmail.com>
2023-02-25 14:03:01 +0100

    Finish main chronometer and work on bonus

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,49 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    start(printTimeCallback) {
+      this.intervalId = setInterval(() => {
+       this.currentTime++;
+       if (typeof printTimeCallback === 'function') {
+         printTimeCallback();
+       //   clearInterval(this.currentTime);
+       // } 
+       
+       }  },1)
+     }
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60000);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return (this.currentTime % 60000) / 1000;
   }
 
   getCentiseconds() {
-    // ... your code goes here
+    let centi = (this.currentTime % 60000) / 1000;
+    return  Math.floor((centi % 2) * 1000)
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) return ('0' + value)
+    else return (String(value))
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    return this.computeTwoDigitNumber(this.getMinutes()) + ":" + this.computeTwoDigitNumber(this.getSeconds());
   }
 }
